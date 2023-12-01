8cee4ed7ce998610d5f37f5b2bf6c3fe6c78985a
Zmeelov7598 <Zmeelov7598@yandex.ru>
2023-12-01 13:35:25 +0600

    Finish Example002 and Sarting Example003

---
+++ DigitalProfessionsFastStartProgramming/IntroductionToTheC#ProgrammingLanguage/Seminare001/Example002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число : ");
+int firstUserNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число : ");
+int secondUserNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(firstUserNumber + "  " + secondUserNumber);
+if (firstUserNumber / secondUserNumber == secondUserNumber)
+{
+    Console.WriteLine("Первое число является квадратом второго! ");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго!");
+}

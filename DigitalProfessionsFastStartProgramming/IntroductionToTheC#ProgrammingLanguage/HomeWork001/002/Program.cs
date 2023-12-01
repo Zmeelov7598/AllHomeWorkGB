8b3c6e8f532893ac3e11b72381186506ee559794
Zmeelov7598 <Zmeelov7598@yandex.ru>
2023-12-01 14:43:09 +0600

    Finish 003

---
+++ DigitalProfessionsFastStartProgramming/IntroductionToTheC#ProgrammingLanguage/HomeWork001/002/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число : ");
+int firstUserNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число : ");
+int secondUserNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число : ");
+int thresometUserNumber = Convert.ToInt32(Console.ReadLine());
+
+int max = firstUserNumber;
+
+while (max < secondUserNumber || max < thresometUserNumber)
+{
+    if (secondUserNumber > max) max = secondUserNumber;
+    if (thresometUserNumber > max) max = thresometUserNumber;
+}
+
+Console.WriteLine($"Максимально число это {max}");
+
+
+
+
+// Второй вариант
+// Console.WriteLine("Введите первое число!");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число!");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число!");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int Max = a;
+// if (b > Max)
+// {
+//     Max = b;
+// }
+// else if (c > Max)
+// {
+//     Max = c;
+// }
+// Console.WriteLine($"Max={Max}");

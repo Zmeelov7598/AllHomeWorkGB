b3ca3dfa8fbfa904cef036fefd46bd7a3c08e834
Zmeelov7598 <Zmeelov7598@yandex.ru>
2023-12-01 14:49:26 +0600

    Finish 003

---
+++ DigitalProfessionsFastStartProgramming/IntroductionToTheC#ProgrammingLanguage/HomeWork001/003/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+//  является ли число чётным (делится ли оно на два без остатка).
+Console.Write("Введите число : ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if (userNumber % 2 == 0) Console.WriteLine("Это Четное число");
+else Console.WriteLine("Это НЕ четное число!");
+Console.WriteLine();

04cce4cf8d2bb8afaa0d557637a7f03882880150
Zmeelov7598 <Zmeelov7598@yandex.ru>
2023-12-01 13:23:52 +0600

    Finished Example001

--- DigitalProfessionsFastStartProgramming/IntroductionToTheC#ProgrammingLanguage/Seminare001/Example001/Program.cs
+++ DigitalProfessionsFastStartProgramming/IntroductionToTheC#ProgrammingLanguage/Seminare001/Example001/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(userNumber);
+Console.WriteLine("Квадрат числа равен : " + userNumber * userNumber);

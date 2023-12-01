e0d917909734939d0ed561bf68938369bfd5f1bc
Zmeelov7598 <Zmeelov7598@yandex.ru>
2023-12-01 13:51:28 +0600

    Finish Seminar001

--- DigitalProfessionsFastStartProgramming/IntroductionToTheC#ProgrammingLanguage/Seminare001/Example003/Program.cs
+++ DigitalProfessionsFastStartProgramming/IntroductionToTheC#ProgrammingLanguage/Seminare001/Example003/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string a = "Понедельник";
+string b = "Вторник";
+string c = "Среда";
+string d = "Четверг";
+string e = "Пятница";
+string f = "Суббота";
+string j = "Воскресенье";
+
+Console.Write("Введите номер дня недели : ");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+if (dayWeek == 1) Console.WriteLine(a);
+if (dayWeek == 2) Console.WriteLine(b);
+if (dayWeek == 3) Console.WriteLine(c);
+if (dayWeek == 4) Console.WriteLine(d);
+if (dayWeek == 5) Console.WriteLine(e);
+if (dayWeek == 6) Console.WriteLine(f);
+if (dayWeek == 7) Console.WriteLine(j);

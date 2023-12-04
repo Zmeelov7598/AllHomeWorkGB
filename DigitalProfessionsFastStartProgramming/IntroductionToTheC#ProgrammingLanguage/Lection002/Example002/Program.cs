55269d3d538557212091e44b855f500716fdd701
Zmeelov7598 <Zmeelov7598@yandex.ru>
2023-12-04 13:47:08 +0600

    I was finished lection002

---
+++ DigitalProfessionsFastStartProgramming/IntroductionToTheC#ProgrammingLanguage/Lection002/Example002/Program.cs
@@ -0,0 +1,25 @@
+﻿int Max(int arq1, int arq2, int arq3)
+{
+    int result = arq1;
+    if (arq2 > result) result = arq2;
+    if (arq3 > result) result = arq3;
+    return result;
+}
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 2311;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23111;
+int c3 = 313;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+Console.WriteLine(max);
---
+++ DigitalProfessionsFastStartProgramming/IntroductionToTheC#ProgrammingLanguage/Lection002/Example003/Program.cs
@@ -0,0 +1,12 @@
+﻿int Max(int arq1, int arq2, int arq3)
+{
+    int result = arq1;
+    if (arq2 > result) result = arq2;
+    if (arq3 > result) result = arq3;
+    return result;
+}
+
+int[] array = { 11, 211, 31, 41, 15, 61, 17, 18, 19 };
+
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[6], array[8]));
+Console.WriteLine(max);

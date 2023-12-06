6881204ae9db96cc00821af3cc3df74d96c610ae
Zmeelov7598 <Zmeelov7598@yandex.ru>
2023-12-06 15:16:21 +0600

    Finish Seminare001

---
+++ DigitalProfessionsFastStartProgramming/IntroductionToTheC#ProgrammingLanguage/Seminare003/Example002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принемает 
+// на вход координаты двух точек и находит растояние между ними в 2D пространстве.
+
+double GetCoordinate(string name, int number)
+{
+    Console.Write($"Введите координату {name} для {number} точки : ");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double x1 = GetCoordinate("X", 1);
+double y1 = GetCoordinate("Y", 1);
+double x2 = GetCoordinate("X", 2);
+double y2 = GetCoordinate("Y", 2);
+
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine(distance);
---
+++ DigitalProfessionsFastStartProgramming/IntroductionToTheC#ProgrammingLanguage/Seminare003/Example003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принемает одно число и выдает таблицу квадратов до этого числа.
+
+int GetNumber()
+{
+    Console.Write("Введите число : ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GetPrint(int number)
+{
+    int count = 1;
+    while (count <= number)
+    {
+        Console.Write($"{Math.Pow(count, 2)}, ");
+        count++;
+    }
+}
+
+GetPrint(GetNumber());

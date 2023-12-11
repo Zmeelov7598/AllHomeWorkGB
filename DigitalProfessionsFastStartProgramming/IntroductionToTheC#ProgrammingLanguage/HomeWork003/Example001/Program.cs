a4f9ca3cf6e86836601e1869ec78c611d20f5e0c
Zmeelov7598 <Zmeelov7598@yandex.ru>
2023-12-11 15:24:08 +0600

    I have finished homework003

---
+++ DigitalProfessionsFastStartProgramming/IntroductionToTheC#ProgrammingLanguage/HomeWork003/Example001/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int GetNumber()
+{
+    Console.Write("Введите 5-ти значное число : ");
+
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+
+    while (userNumber < 9999 || userNumber > 99999)
+    {
+        Console.WriteLine("Вы Ввели не 5-ти значное число!!");
+        Console.WriteLine();
+        Console.Write("Введите 5-ти значное число : ");
+        userNumber = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return userNumber;
+}
+
+void CheckPalindrom(int number)
+{
+    int reverNumber = 0;
+    int userNumber = number;
+    while (reverNumber <= 9999)
+    {
+        int ost = userNumber % 10;
+        reverNumber = (reverNumber * 10) + ost;
+        userNumber = userNumber / 10;
+    }
+    if (reverNumber == number)
+    {
+        Console.WriteLine("Да это палиндром! ");
+    }
+    else
+    {
+        Console.WriteLine("НЕТ это не палиндром! ");
+    }
+}
+
+
+int userNumber = GetNumber();
+
+CheckPalindrom(userNumber);
---
+++ DigitalProfessionsFastStartProgramming/IntroductionToTheC#ProgrammingLanguage/HomeWork003/Example003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int GetNumber()
+{
+    Console.Write("Введите число : ");
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    return userNumber;
+}
+
+void GetCube(int number)
+{
+
+    for (int count = 1; count <= number; count++)
+    {
+        Console.Write($"{Math.Pow(count, 3)}, ");
+    }
+}
+
+int number = GetNumber();
+GetCube(number);

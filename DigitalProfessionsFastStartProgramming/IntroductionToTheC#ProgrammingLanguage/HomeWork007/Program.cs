909ad62c35fbfa4ec02d933db6951795f2257da3
Zmeelov7598 <Zmeelov7598@yandex.ru>
2024-02-05 20:34:28 +0600

    processing Example007 in Seminare007

---
+++ DigitalProfessionsFastStartProgramming/IntroductionToTheC#ProgrammingLanguage/HomeWork007/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int GetArrayLength(string name)
+{
+    Console.Write($"Введите количество {name} : ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j != array.GetLength(1) - 1)
+            {
+                Console.Write($"{array[i, j]}, ");
+            }
+            else
+            {
+                Console.Write(array[i, j]);
+            }
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+int[,] userArray = new int[GetArrayLength("строк"), GetArrayLength("столбцов")];
+
+FillArray(userArray);
+PrintArray(userArray);
+
